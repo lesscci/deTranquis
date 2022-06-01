f525ed4bbc3b432851eb464958e537a76f61cce1
lesscci <102358696+lesscci@users.noreply.github.com>
2022-06-01 13:36:06 +0200

    Arreglado footer y nav

---
+++ js/api.js
@@ -0,0 +1,42 @@
+
+const elBorne = document.querySelectorAll(".el-borne");
+
+
+
+const obtenerPersonajes2 = () => {
+    fetch("https://rickandmortyapi.com/api/character",{
+        method:"GET"
+    })
+    .then(e => e.json())
+    .then(e => recorrerBares(e.results))
+    .catch(e => console.log(e));
+};
+
+
+
+obtenerPersonajes2();
+
+elBorne.forEach(elemento=>console.log(elemento))
+
+const recorrerBares = (bares) => {
+    bares.map(bar=>{
+        elBorne.forEach(elemento=>{
+        const contenedorBar = document.createElement("div");
+        const imagenBar = document.createElement("img");
+        const tituloBar = document.createElement("p");
+        imagenBar.src = bar.image;
+        tituloBar.innerHTML = bar.name;
+        contenedorBar.appendChild(imagenBar);
+        contenedorBar.appendChild(tituloBar);
+          elemento.appendChild(contenedorBar);  
+        })
+    })
+}
+
+/*
+  <div class="media-element" >
+    <img src="/images/bar1.jpg" alt="">
+    <p class="title">
+      Dr.Stravinsky</p>
+  </div>
+*/
---
+++ js/footer.js
@@ -0,0 +1,41 @@
+const Footer = document.querySelector(".footer");
+
+const crearFooter = () => {
+  Footer.innerHTML = ` 
+   <div class="footer__container container">                 
+    <div class="footer__links"> 
+        <p class="footer">Sobre Nosotros</p>
+        <a href="" class="footer__link">Contáctanos</a>
+        <a href="" class="footer__link">Cómo funciona</a>
+        <a href="" class="footer__link">Añade tu bar</a>
+        <a href="" class="footer__link">Únete</a>
+    </div>
+<section class="social__media">
+    <div class="social__media--wrap">
+      <div class="footer__logo">
+        <a href="/" id="footer__logo">DE TRANQUIS</a>
+      </div>
+      <p class="website__rights">© DE TRANQUIS 2022. All rights reserved</p>
+      <div class="social__icons">
+        <a href="/" class="social__icon--link" target="_blank"
+          ><i class="fab fa-facebook"></i
+        ></a>
+        <a href="/" class="social__icon--link"
+          ><i class="fab fa-instagram"></i
+        ></a>
+        <a href="/" class="social__icon--link"
+          ><i class="fab fa-youtube"></i
+        ></a>
+        <a href="/" class="social__icon--link"
+          ><i class="fab fa-linkedin"></i
+        ></a>
+        <a href="/" class="social__icon--link"
+          ><i class="fab fa-twitter"></i
+        ></a>
+      </div>
+    </div>
+  </section>
+</div>`;
+};
+
+crearFooter();
